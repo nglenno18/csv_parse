4a4b692e960d63eddefbf84db46e1dacd8641cd9
Nolan Aloysius Glennon <nglenno18@gmail.com>
2017-02-13 14:43:39 -0600

    config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
 
-// if (env === 'development' || env === 'test') {
+if (env === 'development' || env === 'test') {
   var config = require('./config.json');
   var envConfig = config[env];
   // console.log(envConfig);
@@ -8,4 +8,4 @@ var env = process.env.NODE_ENV || 'development';
   Object.keys(envConfig).forEach((key)=>{
     process.env[key] = envConfig[key];
   });
-// }
+}
